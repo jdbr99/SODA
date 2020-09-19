5462e894fba95828b25006e4e82088493a4bdb1c
David Bolaños <jdbr@localhost.localdomain>
2020-09-19 13:12:39 -0500

    Home route working (finally :D )

--- app/views.py
+++ app/views.py
@@ -2,29 +2,51 @@
 Declaration of views and routes.
 '''
 from flask import render_template, request
+from pathlib import Path
 from app import app
+from datetime import datetime
+import os
+import dotenv
 
 config = {
     "bg_color": "#dcdcdc"
 }
 
+def parse_tags(entry):
+    file_path = Path(f'{entry}/tags.txt')
+    with open(file_path) as f:
+        return list(f)
+
+def get_description(entry):
+    file_path = Path(f'{entry}/description.txt')
+    with open(file_path) as f:
+        return f.read()
+
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
 def home():
-    lipsum = '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu accumsan quam. Integer vel mauris lacus. Vestibulum arcu augue, hendrerit ac enim at, rhoncus mattis lorem. Suspendisse sit amet ex a dui ornare efficitur. In faucibus justo eget magna pretium viverra. Etiam ac condimentum lectus. Proin rhoncus fringilla eros, ac auctor sapien.
+    base_dir = dotenv.get_key(".env", "DIR")
+    base_dir = Path(f'{base_dir}/soda_files/data_sources')
+    _, dirnames, _ = next(os.walk(base_dir))
 
-Nullam id elit ipsum. Maecenas sollicitudin tincidunt leo id euismod. Nullam scelerisque vel dui ut sollicitudin. Praesent sit amet mi tortor. Morbi elementum ex ac odio dignissim eleifend. Vivamus at ipsum at neque tempor vehicula. Nullam nec ante et neque efficitur pharetra. In hac habitasse platea dictumst. Integer ut diam vitae dolor pellentesque fringilla id at augue. Nulla nec fermentum orci. Fusce quis scelerisque orci, quis suscipit lorem. Nullam vitae urna dui. Fusce pulvinar, felis vitae faucibus facilisis, felis augue viverra dui, ac sagittis nibh velit eget arcu. Sed tristique hendrerit scelerisque.
+    recent = []
 
-Etiam vitae magna risus. Cras placerat dictum mauris, ut dapibus nisi consequat dapibus. Morbi iaculis ligula arcu, et commodo purus tempus fringilla. Duis pulvinar nisl sit amet arcu finibus, ut dictum mauris congue. Mauris at ornare quam. Nullam nisi lacus, venenatis viverra velit at, dapibus iaculis risus. Pellentesque nec quam nec elit feugiat placerat. In et odio dignissim, sagittis dui a, interdum lectus. Nulla facilisi. Integer at consectetur erat. Sed luctus iaculis mauris. Fusce mollis interdum lectus, ac bibendum metus pellentesque vel. Suspendisse ipsum purus, aliquam vel est venenatis, volutpat pharetra dui. Donec maximus, arcu at lobortis laoreet, magna odio pharetra quam, at dapibus nibh mauris quis orci. Suspendisse felis tortor, accumsan sit amet mauris quis, mollis cursus augue.
+    for entry in dirnames:
+        for path, dirs, files in os.walk(base_dir):
+            if path.endswith(entry):
+                tags = parse_tags(path)
+                description = get_description(path)
+                timestamp = datetime.fromtimestamp(os.path.getmtime(path))
+                last_modified = timestamp.strftime("%m/%d/%y - %H:%M")
+                recent.append({
+                    "name": entry,
+                    "tags": tags,
+                    "description": description,
+                    "timestamp": timestamp,
+                    "last_updated": last_modified
+                })
 
-Donec sodales metus eu metus pellentesque, sit amet congue urna vestibulum. Donec efficitur, mauris et pellentesque gravida, erat velit aliquam nunc, eget consequat erat enim eu dui. Integer condimentum metus in laoreet interdum. Morbi congue libero arcu, et feugiat erat iaculis sed. Suspendisse eu nulla urna. Etiam varius lacus vitae elit tincidunt, ut efficitur ante dignissim. Aliquam fringilla maximus tortor ac finibus. Donec et lorem ipsum. Donec pulvinar luctus posuere. Mauris eu egestas eros. Aenean viverra dignissim risus, fringilla porttitor ante.
+    recent.sort(key=lambda entry : entry["timestamp"], reverse=True)
 
-Integer non erat a lectus bibendum varius. Curabitur semper quam ante, non pulvinar mauris semper id. Curabitur condimentum eros scelerisque velit posuere, finibus porttitor ante feugiat. Donec hendrerit iaculis lacinia. Quisque vitae tristique metus, id ornare massa. Praesent velit nunc, viverra pulvinar aliquet sit amet, sodales non elit. Duis lobortis euismod libero ac euismod. Donec at lacus mollis magna congue auctor vitae nec sem. Vivamus in bibendum diam. In hac habitasse platea dictumst. Mauris nec lacus ut felis rhoncus sagittis a non orci. Morbi ultrices volutpat blandit. '''
-    test = [
-        {"name": "Entry #1", "description": "ja;ldskjflakdj;flakjdf", "tags": ["tag1", "tag2"], "formats": ["csv", "xlsx", "REST API"], "last_updated": "Sep 19, 10:10"},
-        {"name": "Entry #2", "description": lipsum, "tags": ["tag1", "tag2"], "formats": ["csv", "xlsx", "REST API"], "last_updated": "Sep 19, 10:10"},
-        {"name": "Entry #3", "description": lipsum, "tags": ["tag2", "tag3"], "formats": ["csv", "xlsx", "REST API"], "last_updated": "Sep 19, 10:10"}
-    ]
-    return render_template('index.html', recent=test, config=config)
+    return render_template('index.html', recent=recent, config=config)
