55d06357b438595304750a9f1fa7d241e128323f
SantuarioDeRosas <SantuarioDeRosas@users.noreply.github.com>
2020-09-19 17:44:54 -0500

    Added a BETTER search route (a much "perrón" one).

--- app/views.py
+++ app/views.py
@@ -26,7 +26,28 @@ def get_file_sources(entry):
     file_path = Path(f'{entry}/files')
     _, _, files = next(os.walk(file_path))
     return files
-        
+
+
+def tags_score(evaluation_tags, testing_tags):
+    score = 0
+    for e_tag in evaluation_tags:
+        for t_tag in testing_tags:
+            if e_tag == t_tag:
+                score += 10
+
+    return score
+
+
+def tags_filter(tags):
+    new_tags = []
+    for tag in tags:
+        temp = tag
+        if tag.endswith("\n"):
+            temp = tag[:-1]
+        new_tags.append(temp)
+
+    return new_tags
+
 
 @app.route('/')
 @app.route('/home')
@@ -68,14 +89,14 @@ def data_source():
         return redirect('/')
 
     name = args['q']
-    
+
     base_dir = dotenv.get_key(".env", "DIR")
     base_dir = Path(f'{base_dir}/soda_files/data_sources')
     _, dirnames, _ = next(os.walk(base_dir))
 
     if name not in dirnames:
         return "404"
-   
+
     for path, _, _ in os.walk(base_dir):
         if path.endswith(name):
             tags = parse_tags(path)
@@ -95,3 +116,38 @@ def data_source():
 
     print(data['files'])
     return render_template('data_source.html', data=data, config=config)
+
+
+@app.route('/search')
+def search():
+    _dir_base = Path(os.getcwd())
+
+    # We obtain the direction for the data folders
+    data_source_dir = _dir_base/"soda_files"/"data_sources"
+    os.chdir(data_source_dir)
+    data_source_list = os.listdir()
+
+    # A little bit of hard coding
+    evaluation_tags = request.args["q"]
+    # We split in a different line in case some cleaning is required
+    evaluation_tags = evaluation_tags.split()
+
+    priority_folders = []
+
+    for data_folder_name in data_source_list:
+        current = data_source_dir/data_folder_name
+        tags = tags_filter(parse_tags(current))
+        score = tags_score(evaluation_tags, tags)
+        timestamp = datetime.fromtimestamp(os.path.getmtime(current))
+        last_modified = timestamp.strftime("%m/%d/%y - %H:%M")
+        priority_folders.append({
+            "name": data_folder_name,
+            "tags": tags,
+            "score": score,
+            "last_updated": last_modified
+        })
+
+    priority_folders.sort(key=lambda entry: entry["score"], reverse=True)
+
+    return "Buscado"
+
