665214245f0acb2e817ec2e0a5de86a5d23cef54
David Bolaños <jdbr@localhost.localdomain>
2020-09-19 17:15:15 -0500

    Data source route and views done

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 '''
 Declaration of views and routes.
 '''
-from flask import render_template, request
+from flask import render_template, request, redirect
 from pathlib import Path
 from app import app
 from datetime import datetime
@@ -21,6 +21,12 @@ def get_description(entry):
     file_path = Path(f'{entry}/description.txt')
     with open(file_path) as f:
         return f.read()
+
+def get_file_sources(entry):
+    file_path = Path(f'{entry}/files')
+    _, _, files = next(os.walk(file_path))
+    return files
+        
 
 @app.route('/')
 @app.route('/home')
@@ -54,3 +60,38 @@ def home():
     recent.sort(key=lambda entry : entry["timestamp"], reverse=True)
 
     return render_template('index.html', recent=recent, config=config)
+
+@app.route('/data_source')
+def data_source():
+    args = request.args
+    if(not bool(args)):
+        return redirect('/')
+
+    name = args['q']
+    
+    base_dir = dotenv.get_key(".env", "DIR")
+    base_dir = Path(f'{base_dir}/soda_files/data_sources')
+    _, dirnames, _ = next(os.walk(base_dir))
+
+    if name not in dirnames:
+        return "404"
+   
+    for path, _, _ in os.walk(base_dir):
+        if path.endswith(name):
+            tags = parse_tags(path)
+            description = get_description(path)
+            files_list = get_file_sources(path)
+            # files_processed = [filename.split('.') for filename in files_list]
+            timestamp = datetime.fromtimestamp(os.path.getmtime(path))
+            last_modified = timestamp.strftime("%m/%d/%y - %H:%M")
+            data = {
+                "name": name,
+                "tags": tags,
+                "description": description,
+                "files": files_list,
+                "timestamp": timestamp,
+                "last_updated": last_modified,
+            }
+
+    print(data['files'])
+    return render_template('data_source.html', data=data, config=config)
